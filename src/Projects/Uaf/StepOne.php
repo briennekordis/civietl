94a1e3310244c7812f7c05d9944786f48d11b670
Jon Goldberg <jon@megaphonetech.com>
2023-03-13 11:18:41 -0400

    rename Contacts to StepOne, add a couple of fields

--- src/Projects/Uaf/StepOne.php
+++ src/Projects/Uaf/StepOne.php
@@ -3,7 +3,7 @@ namespace Civietl\Projects\Uaf;
 
 use Civietl\Transforms as T;
 
-class Contacts {
+class StepOne {
 
   /**
    * Do all the transforms associated with this step.
@@ -19,13 +19,20 @@ class Contacts {
       'Constituent Type' => 'contact_type',
       'Prefix' => 'prefix_id:label',
       'Suffix' => 'suffix_id:label',
+      'Job Title' => 'job_title',
+      'Deceased?' => 'is_deceased',
     ]);
+    // Clean up prefix IDs.
     $rows = T\ValueTransforms::valueMapper($rows, 'prefix_id:label', \Civietl\Maps::PREFIX_MAP);
     // Create any missing prefixes in the option values table.
     $prefixes = T\RowFilters::getUniqueValues($rows, 'prefix_id:label');
     T\CiviCRM::createOptionValues('individual_prefix', $prefixes);
+    // Clean up suffix IDs.
+    $rows = T\ValueTransforms::valueMapper($rows, 'suffix_id:label', \Civietl\Maps::SUFFIX_MAP);
+    $suffixes = T\RowFilters::getUniqueValues($rows, 'suffix_id:label');
+    T\CiviCRM::createOptionValues('individual_suffix', $suffixes);
     // For testing - just show 5 rows.
-    $rows = T\RowFilters::randomSample($rows, 5);
+    // $rows = T\RowFilters::randomSample($rows, 5);
     return $rows;
   }
 
