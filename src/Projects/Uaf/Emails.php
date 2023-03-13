ce0a4e6092e097df8eabff368070d11572b5179e
Jon Goldberg <jon@megaphonetech.com>
2023-03-13 15:49:07 -0400

    work on UAF website and emails

--- src/Projects/Uaf/Emails.php
+++ src/Projects/Uaf/Emails.php
@@ -3,30 +3,28 @@ namespace Civietl\Projects\Uaf;
 
 use Civietl\Transforms as T;
 
-class Websites {
+class Emails {
 
   /**
    * Do all the transforms associated with this step.
    */
   public function transforms(array $rows) : array {
+    // Trim and lowercase emails.
+    $rows = T\Text::lowercase($rows, ['Email']);
+    $rows = T\Text::trim($rows, ['Email']);
+    // Log and remove invalid email addresses.
+    $rows = T\Cleanup::filterInvalidEmails($rows, 'Email');
     // Delete unused columns. Not necessary but easier (and marginally faster) to work with.
-    $rows = T\Columns::deleteColumns($rows, ['LGL Website ID', 'Constituent Name']);
+    $rows = T\Columns::deleteColumns($rows, ['LGL Email ID', 'Constituent Name', 'Is Valid']);
     // Rename some columns that are one-to-one with Civi.
     $rows = T\Columns::renameColumns($rows, [
       'LGL Constituent ID' => 'external_identifier',
-      'URL' => 'url',
-      'Website Type' => 'website_type_id:label',
+      'Email' => 'email',
+      'Email Type' => 'location_type_id:label',
     ]);
-    // Trim and lowercase URLs.
-    $rows = T\Text::lowercase($rows, ['url']);
-    $rows = T\Text::trim($rows, ['url']);
-    // Add protocol if necessary.
-    $rows = T\Cleanup::addUrlProtocol($rows, 'url');
-    // Website type ID: "Website" to "Main".
-    $rows = T\ValueTransforms::valueMapper($rows, 'website_type_id:label', ['Website' => 'Main']);
     // Create any missing website types in the option values table.
-    $websiteTypes = T\RowFilters::getUniqueValues($rows, 'website_type_id:label');
-    T\CiviCRM::createOptionValues('website_type', $websiteTypes);
+    $locationTypes = T\RowFilters::getUniqueValues($rows, 'location_type_id:label');
+    T\CiviCRM::createLocationTypes($locationTypes);
     $rows = T\CiviCRM::lookup($rows, 'Contact', 'external_identifier', ['id']);
     $rows = T\Columns::renameColumns($rows, ['id' => 'contact_id']);
     return $rows;
