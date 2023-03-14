f17c61e6cd8c87d747b2bf458c3d7e67e171a5a9
Jon Goldberg <jon@megaphonetech.com>
2023-03-13 20:46:36 -0400

    Changes for Monolog 1.x compat

--- src/Logging.php
+++ src/Logging.php
@@ -1,7 +1,6 @@
 <?php
 namespace Civietl;
 
-use Monolog\Level;
 use Monolog\Logger;
 use Monolog\Handler\StreamHandler;
 
@@ -12,7 +11,7 @@ class Logging {
   public static function setLogFolder(string $logFolder) {
     self::$logFolder = $logFolder;
     self::$logger = new Logger('civietl');
-    self::$logger->pushHandler(new StreamHandler(self::$logFolder . '/errors.log', Level::Debug));
+    self::$logger->pushHandler(new StreamHandler(self::$logFolder . '/errors.log', Logger::INFO));
     self::$logger->info('Begin logging.');
   }
 
