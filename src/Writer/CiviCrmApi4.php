0158a953b317aa23650ab0615c982be3eba006ba
Jon Goldberg <jon@megaphonetech.com>
2023-03-14 18:02:22 -0400

    CiviCrmApi4 writer now properly logs API errors, with row headers

--- src/Writer/CiviCrmApi4.php
+++ src/Writer/CiviCrmApi4.php
@@ -7,6 +7,10 @@ class CivicrmApi4 implements WriterInterface {
   private string $primaryEntity;
   private bool $allowDuplicates;
   private array $matchFields;
+  /**
+   * @var bool This is so we know whether to print the row headers.
+   */
+  private bool $errorFound = FALSE;
 
   public function __construct($options) {
     $this->primaryEntity = $options['civi_primary_entity'];
@@ -14,33 +18,42 @@ class CivicrmApi4 implements WriterInterface {
     $this->matchFields = $options['match_fields'] ?? ['id'];
   }
 
-  public function writeOne($row) : array {
+  public function writeOne($row) : void {
     $logEntry = ['Error' => FALSE];
-    if ($this->allowDuplicates) {
-      $result = civicrm_api4($this->primaryEntity, 'create', [
-        'checkPermissions' => FALSE,
-        'values' => $row,
-      ]);
+    try {
+      if ($this->allowDuplicates) {
+        $result = civicrm_api4($this->primaryEntity, 'create', [
+          'checkPermissions' => FALSE,
+          'values' => $row,
+        ]);
+      }
+      else {
+        $result = civicrm_api4($this->primaryEntity, 'save', [
+          'checkPermissions' => FALSE,
+          'records' => [$row],
+          'match' => $this->matchFields,
+        ]);
+      }
+      if ($result['error_message'] ?? FALSE) {
+        Logging::log("Failed to import: $row");
+        Logging::log("Error: $result");
+      }
     }
-    else {
-      $result = civicrm_api4($this->primaryEntity, 'save', [
-        'checkPermissions' => FALSE,
-        'records' => [$row],
-        'match' => $this->matchFields,
-      ]);
+    catch (\CRM_Core_Exception $e) {
+      if (!$this->errorFound) {
+        $this->errorFound = TRUE;
+        Logging::log("Failed to import $this->primaryEntity");
+        Logging::log(implode(', ', array_keys($row)));
+      }
+      Logging::log(implode(', ', $row));
+      Logging::log("Error: " . $e->getMessage());
     }
-    if ($result['error_message'] ?? FALSE) {
-      Logging::log("Failed to import: $row");
-      Logging::log("Error: $result");
-    }
-    return $logEntry;
   }
 
-  public function writeAll($rows) : array {
+  public function writeAll($rows) : void {
     foreach ($rows as $row) {
-      $logEntries[] = $this->writeOne($row);
+      $this->writeOne($row);
     }
-    return $logEntries;
   }
 
 }
