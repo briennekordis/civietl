d95cc8eb49eea173ba3fae1b1371065e2d1b2348
Jon Goldberg <jon@megaphonetech.com>
2023-03-13 20:47:11 -0400

    filter blanks transform

--- src/Transforms/RowFilters.php
+++ src/Transforms/RowFilters.php
@@ -22,4 +22,34 @@ class RowFilters {
     return $newRows;
   }
 
+  /**
+   * Get a random sample of rows for testing.
+   */
+  public static function filterBlanks(array $rows, string $columnName) : array {
+    foreach ($rows as $key => $row) {
+      if (empty($row[$columnName])) {
+        unset($rows[$key]);
+      }
+    }
+    return $rows;
+  }
+
+  /**
+   * Filter out records we don't want.
+   * @var filters
+   * Filters is a two-dimensional array, the inner array has the following values:
+   * string columnName
+   * string operator (one of: =, !=, is empty, is not empty, >=, >, <, <=, between)
+   * mixed value (for operators that use them)
+   * mixed value2 (for between operator)
+   */
+  // public static function filter(array $rows, array $filters) : array {
+  //   foreach ($filters as $filter) {
+  //     $columnName = $filter[0];
+  //     $operator = $filter[1];
+  //     $value = $filter[2] ?? NULL;
+  //     $value2 = $filter[3] ?? NULL;
+  //   }
+  // }
+
 }
