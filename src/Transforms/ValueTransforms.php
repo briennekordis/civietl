edb238e4682386c2edbdb215acf3c8f5d62fcaeb
Jon Goldberg <jon@megaphonetech.com>
2023-03-13 20:47:38 -0400

    allow value mapper to map to a different column

--- src/Transforms/ValueTransforms.php
+++ src/Transforms/ValueTransforms.php
@@ -8,11 +8,14 @@ class ValueTransforms {
    * E.g. ['New Yrok' => 'NY', 'N.Y.' => 'NY', etc.].
    * @var mapping
    *   array in format `['oldvalue1' => 'newvalue1', 'oldvalue2' => 'newvalue2', etc.]`
+   * @var newColumn
+   *   If NULL, map the new values over the existing values.  If not NULL, put the new values in $newColumn.
    */
-  public static function valueMapper(array $rows, string $columnName, array $mapping) : array {
+  public static function valueMapper(array $rows, string $columnName, array $mapping, ?string $newColumn = NULL) : array {
+    $newColumn ??= $columnName;
     foreach ($rows as &$row) {
       if ($mapping[$row[$columnName]] ?? FALSE) {
-        $row[$columnName] = $mapping[$row[$columnName]];
+        $row[$newColumn] = $mapping[$row[$columnName]];
       }
     }
     return $rows;
