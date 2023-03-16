c83327e3a53e3c07dc08a7f539a07f27a52e7968
Brienne Kordis <brienne@megaphonetech.com>
2023-03-16 16:35:03 -0400

    add Contributions entity

---
+++ src/Projects/Uaf/Contributions.php
@@ -0,0 +1,59 @@
+<?php
+namespace Civietl\Projects\Uaf;
+
+use Civietl\Transforms as T;
+
+class Contributions {
+
+  /**
+   * Do all the transforms associated with this step.
+   */
+  public function transforms(array $rows) : array {
+    // Remove columns that will not be imported.
+    $rows = T\Columns::deleteAllColumnsExcept($rows, [
+      'LGL Constituent ID',
+      'LGL Gift ID',
+      'LGL Parent Gift ID',
+      'LGL Campaign ID',
+      'Fund',
+      'Appeal',
+      'Gift Category',
+      'Gift note',
+      'Gift Amount',
+      'Gift date',
+      'Deposit Date',
+      'Payment type',
+      'Check/Reference No.',
+      'Anonymous gift?',
+      'Gift owner',
+      'Gift batch ID',
+      'Vehicle Name',
+    ]);
+    // Rename the columns that will be imported to match CiviCRM fields.
+    $rows = T\Columns::renameColumns($rows, [
+      'LGL Constituent ID' => 'contact_external_identifier',
+      'LGL Gift ID' => 'Legacy_Contribution_Data.LGL_Gift_ID',
+      // 'LGL Parent Gift ID',
+      'LGL Campaign ID' => 'campaign_external_identifier',
+      'Fund' => 'financial_type_id:label',
+      // 'Appeal' => 'source',
+      // 'Gift Category',
+      'Gift Amount' => 'total_amount',
+      'Gift date' => 'receive_date',
+      'Deposit Date' => 'Additional_Contribution_Data.Deposited_Date',
+      'Payment type' => 'payment_instrument_id.label',
+      'Check/Reference No.' => 'check_number',
+      'Anonymous gift?' => 'Additional_Contribution_Data.Anonymous_gift',
+      // 'Gift owner',
+      // 'Gift batch ID',
+      // 'Vehicle Name',
+    ]);
+    // Look up and reutrn the id of Entities this Contribution is tied to.
+    $rows = T\CiviCRM::lookup($rows, 'Contact', 'external_identifier', 'external_identifier', ['id']);
+    $rows = T\CiviCRM::lookup($rows, 'Campaign', 'campaign_external_identifier', 'external_identifier', ['id']);
+    // Remap true to 1 and false to 0 for is_active.
+    $rows = T\ValueTransforms::valueMapper($rows, 'is_active', ['false' => 0, 'true' => 1]);
+    return $rows;
+  }
+
+}
