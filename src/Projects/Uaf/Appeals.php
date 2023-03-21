4592f489599a8836739c900f3ecc0f7c5d440a8e
Brienne Kordis <brienne@megaphonetech.com>
2023-03-21 17:28:17 -0400

    resolve contraint violation

--- src/Projects/Uaf/Appeals.php
+++ src/Projects/Uaf/Appeals.php
@@ -22,7 +22,7 @@ class Appeals {
     // Rename the columns that will be imported to match CiviCRM fields.
     $rows = T\Columns::renameColumns($rows, [
       'LGL Appeal ID' => 'external_identifier',
-      'LGL Campaign ID' => 'parent_id',
+      'LGL Campaign ID' => 'campaign_external_identifier',
       'Name' => 'title',
       'Description' => 'description',
       'Goal' => 'goal_revenue',
@@ -31,6 +31,10 @@ class Appeals {
     ]);
     // Remap true to 1 and false to 0 for is_active.
     $rows = T\ValueTransforms::valueMapper($rows, 'is_active', ['false' => 0, 'true' => 1]);
+    // Look up the LGL Campaign Id and then rename it as the parent_id.
+    $rows = T\CiviCRM::lookup($rows, 'Campaign', 'campaign_external_identifier', 'external_identifier', ['id']);
+    $rows = T\Columns::renameColumns($rows, ['id' => 'parent_id']);
+
     return $rows;
   }
 
