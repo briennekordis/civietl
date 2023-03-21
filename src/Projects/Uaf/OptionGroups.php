c7887600f69d7c4c0955f0551e583d288882f9d6
Brienne Kordis <brienne@megaphonetech.com>
2023-03-21 14:14:59 -0400

    OptionGroup and OptionValues step

---
+++ src/Projects/Uaf/OptionGroups.php
@@ -0,0 +1,14 @@
+<?php
+namespace Civietl\Projects\Uaf;
+
+class OptionGroups {
+
+  /**
+   * Do all the transforms associated with this step.
+   */
+  public function transforms(array $rows) : array {
+    // No transforms!
+    return $rows;
+  }
+
+}
