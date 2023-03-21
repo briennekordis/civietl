a26dc42ab5c152bafaf24f188c5396c123d6ca33
Brienne Kordis <brienne@megaphonetech.com>
2023-03-21 14:15:08 -0400

    Appeals step

---
+++ src/Projects/Uaf/Appeals.php
@@ -0,0 +1,37 @@
+<?php
+namespace Civietl\Projects\Uaf;
+
+use Civietl\Transforms as T;
+
+class Appeals {
+
+  /**
+   * Do all the transforms associated with this step.
+   */
+  public function transforms(array $rows) : array {
+    // Remove columns that will not be imported.
+    $rows = T\Columns::deleteAllColumnsExcept($rows, [
+      'LGL Appeal ID',
+      'LGL Campaign ID',
+      'Name',
+      'Description',
+      'Goal',
+      'Date',
+      'Is Active?',
+    ]);
+    // Rename the columns that will be imported to match CiviCRM fields.
+    $rows = T\Columns::renameColumns($rows, [
+      'LGL Appeal ID' => 'external_identifier',
+      'LGL Campaign ID' => 'parent_id',
+      'Name' => 'title',
+      'Description' => 'description',
+      'Goal' => 'goal_revenue',
+      'Date' => 'start_date',
+      'Is Active?' => 'is_active',
+    ]);
+    // Remap true to 1 and false to 0 for is_active.
+    $rows = T\ValueTransforms::valueMapper($rows, 'is_active', ['false' => 0, 'true' => 1]);
+    return $rows;
+  }
+
+}
