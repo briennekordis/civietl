f5078794a030c2008c70f7bdac9633dad5c41bb6
Brienne Kordis <brienne@megaphonetech.com>
2023-03-22 18:48:17 -0400

    wip of StepTwo

---
+++ src/Projects/Uaf/StepTwo.php
@@ -0,0 +1,30 @@
+<?php
+namespace Civietl\Projects\Uaf;
+
+use Civietl\Transforms as T;
+
+class StepTwo {
+
+  /**
+   * Do all the transforms associated with this step.
+   */
+  public function transforms(array $rows) : array {
+    $rows = T\Columns:: deleteAllColumnsExcept($rows, [
+      'LGL Constituent ID',
+      'Spouse Name',
+      'Spouse Nick Name',
+    ]);
+    // Rename some columns that are one-to-one with Civi.
+    $rows = T\Columns::renameColumns($rows, [
+      'LGL Constituent ID' => 'external_identifier',
+    ]);
+    // Split Spouses into separate contact records.
+    $rows = T\Cleanup::splitSpouses($rows, 'external_identifier');
+    // Split the Spouse Name into separate strings.
+    $rows = T\Text::stringSplit($rows, 'Spouse Name');
+    // For testing - just show 5 rows.
+    // $rows = T\RowFilters::randomSample($rows, 5);
+    return $rows;
+  }
+
+}
