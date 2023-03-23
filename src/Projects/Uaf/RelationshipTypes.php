cc851ea533ec1af8e695c61d445e705df86ed639
Jon Goldberg <jon@megaphonetech.com>
2023-03-23 14:31:12 -0400

    RelationshipTypes

---
+++ src/Projects/Uaf/RelationshipTypes.php
@@ -0,0 +1,13 @@
+<?php
+namespace Civietl\Projects\Uaf;
+
+class RelationshipTypes {
+
+  /**
+   * Do all the transforms associated with this step.
+   */
+  public function transforms(array $rows) : array {
+    return $rows;
+  }
+
+}
