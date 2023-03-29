6f8519a946352b7cf2d7be7f2c88f33bb0c713ac
Jon Goldberg <jon@megaphonetech.com>
2023-03-29 19:57:50 -0400

    finish country mapping

--- src/Projects/Uaf/Addresses.php
+++ src/Projects/Uaf/Addresses.php
@@ -6,17 +6,56 @@ use Civietl\Transforms as T;
 class Addresses {
 
   public function transforms(array $rows) : array {
-    $rows = T\CiviCRM::lookup($rows, 'Contact', 'LGL Constituent ID', 'external_identifier', ['id']);
+    // Get Contact ID.
+    $rows = T\CiviCRM::lookup($rows, 'Contact', 'LGL Constituent ID', ['external_identifier'], ['id']);
     $rows = T\Columns::renameColumns($rows, [
       'id' => 'contact_id',
       'Address Type' => 'location_type_id:label',
       'City' => 'city',
-      'Postal Code/Zip' => 'postal_code',
-      'Is Preferred' => 'is_primary',
+      'Postal Code/ZIP' => 'postal_code',
+      'Is Preferred?' => 'is_primary',
     ]);
-    $rows = T\Columns::deleteColumns($rows, ['LGL Constituent ID', 'Constituent Name', 'LGL Address ID', 'County', 'Seasonal From', 'Seasonal To', 'Is Valid']);
+    // Split Address into two fields when there's a carriage return.
+    $rows = T\Transform::splitFieldToFields($rows, 'Street', "\n");
+    $rows = T\Columns::renameColumns($rows, [
+      'Street_0' => 'street_address',
+      'Street_1' => 'supplemental_address_1',
+      'Street_2' => 'supplemental_address_2',
+      'Street_3' => 'supplemental_address_3',
+    ]);
+    $rows = T\Columns::deleteColumns($rows, ['LGL Constituent ID', 'Constituent Name', 'LGL Address ID', 'County', 'Seasonal from', 'Seasonal to', 'Is Valid?', 'Street']);
+    // Trim every field.
     $rows = T\Text::trim($rows, array_keys(reset($rows)));
+    // Clean up and map countries. First the ones that are special to this data, then all the rest.
+    $rows = T\ValueTransforms::valueMapper($rows, 'Country', \Civietl\Maps::COUNTRY_MAP);
+    $rows = T\ValueTransforms::valueMapper($rows, 'Country', self::UAF_COUNTRY_MAP);
+    // Set a default country (necessary for doing state cleanups)
+    $rows = T\ValueTransforms::valueMapper($rows, 'Country', ['' => 'United States']);
+    // Do lookups by ISO code and name separately.
+    $rowsWithISOCode = array_filter($rows, function($row) {
+      return strlen($row['Country']) === 2;
+    });
+    $rowsWithISOCode = T\CiviCRM::lookup($rowsWithISOCode, 'Country', 'Country', ['iso_code'], ['id']);
+
+    $rowsWithName = array_diff_key($rows, $rowsWithISOCode);
+    $rowsWithName = T\CiviCRM::lookup($rowsWithName, 'Country', 'Country', ['name'], ['id']);
+    // rejoin the rows.
+    $rows = $rowsWithISOCode + $rowsWithName;
+    $rows = T\Columns::renameColumns($rows, ['id' => 'country_id']);
+
     return $rows;
   }
+
+  const UAF_COUNTRY_MAP = [
+    '10023-5565' => '',
+    'S. America' => '',
+    'B- 1049 Brussels' => 'Belgium',
+    'Harare, Zimbabwe' => 'Zimbabwe',
+    '10032' => 'United States',
+    '94559' => 'United States',
+    '91436-4487' => 'United States',
+    '70115' => 'United States',
+    'Czechia' => 'Czech Republic',
+  ];
 
 }
