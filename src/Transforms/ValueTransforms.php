d854f52a55a2f43fae2f4af7ffba17e88f94a373
Jon Goldberg <jon@megaphonetech.com>
2023-03-30 23:40:00 -0400

    allow case-insensitive value mapping

--- src/Transforms/ValueTransforms.php
+++ src/Transforms/ValueTransforms.php
@@ -11,15 +11,32 @@ class ValueTransforms {
    * @var newColumn
    *   If NULL, map the new values over the existing values.  If not NULL, put the new values in $newColumn.
    */
-  public static function valueMapper(array $rows, string $columnName, array $mapping, ?string $newColumn = NULL) : array {
+  public static function valueMapper(array $rows, string $columnName, array $mapping, ?string $newColumn = NULL, $caseSensitive = TRUE) : array {
     $newColumn ??= $columnName;
+    // Uppercase for case-insensitive search.
+    if (!$caseSensitive) {
+      foreach ($mapping as $oldValue => $newValue) {
+        $newMapping[strtoupper($oldValue)] = $newValue;
+      }
+      $mapping = $newMapping;
+    }
+
     foreach ($rows as &$row) {
       // This ensures there's always a "newColumn" element even if there's no value mapping.
       $row[$newColumn] ??= '';
-      if (array_key_exists($row[$columnName], $mapping)) {
-        $row[$newColumn] = $mapping[$row[$columnName]];
+      if ($caseSensitive) {
+        if (array_key_exists($row[$columnName], $mapping)) {
+          $row[$newColumn] = $mapping[$row[$columnName]];
+        }
+      }
+      else {
+        if (array_key_exists(strtoupper($row[$columnName]), $mapping)) {
+          $row[$newColumn] = $mapping[strtoupper($row[$columnName])];
+        }
       }
     }
+    // Remove dangling reference.
+    unset($row);
     return $rows;
   }
 
