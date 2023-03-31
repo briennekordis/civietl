9cb1a7b7f0bf94b6783c97ec10b20029447d34e6
Brienne Kordis <brienne@megaphonetech.com>
2023-03-31 10:01:32 -0400

    tweaks to MemberRelationships

--- src/Projects/Uaf/MemberRelationships.php
+++ src/Projects/Uaf/MemberRelationships.php
@@ -27,13 +27,13 @@ class MemberRelationships {
     ]);
 
     // Get contact ID A.
-    $rows = T\CiviCRM::lookup($rows, 'Contact', ['LGL Constituent ID' => 'external_identifier'], ['id']);
+    $rows = T\CiviCRM::lookup($rows, 'Contact', ['contact_external_identifier' => 'external_identifier'], ['id']);
     $rows = T\Columns::renameColumns($rows, ['id' => 'contact_id_a']);
     // Set contact ID B to the default Organization (Urgent Action Fund).
     $rows = T\Columns::newColumnWithConstant($rows, 'contact_id_b', 1);
     // Clean up the relationships.
     $rows = T\Columns::newColumnWithConstant($rows, 'is_active', 0);
-    $rows = T\ValueTransforms::valueMapper($rows, 'Relationship Type', ['UAF Board of Directors' => 'Board Member of', 'UAF Staff' => 'Employee of', 'US Advisory Council Members' => 'Advisory Council Member for']);
+    $rows = T\ValueTransforms::valueMapper($rows, 'Relationship Type', ['UAF Board of Directors' => 'Board Member of', 'UAF Staff' => 'Employee of', 'US Advisory Council Members' => 'US Advisory Council Member for']);
     // Get relationship type IDs.
     $rows = T\CiviCRM::lookup($rows, 'RelationshipType', ['Relationship Type' => 'name_a_b'], ['id']);
     $rows = T\Columns::renameColumns($rows, ['id' => 'relationship_type_id']);
