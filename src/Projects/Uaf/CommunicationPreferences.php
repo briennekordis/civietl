f2174c286413b9e0cf0c6c998dd58fe4ff86e0af
Jon Goldberg <jon@megaphonetech.com>
2023-04-07 13:49:02 -0400

    CommunicationPreferences step

--- src/Projects/Uaf/CommunicationPreferences.php
+++ src/Projects/Uaf/CommunicationPreferences.php
@@ -14,54 +14,23 @@ class CommunicationPreferences {
       'LGL Constituent ID',
       'Communication Tags',
       'Acknowledgment Preference',
-      'Annual Report Name',
-      'Addressee',
-      'Salutation',
-      'Alt. Addressee',
-      'Alt. Salutation',
     ]);
     // Rename some columns that are one-to-one with Civi.
     $rows = T\Columns::renameColumns($rows, [
       'LGL Constituent ID' => 'external_identifier',
       'Acknowledgment Preference' => 'preferred_communication_method:label',
-      'Annual Report Name' => 'Annual_Report.Annual_Report_Name',
-      'Alt. Addressee' => 'addressee_custom',
-      'Alt. Salutation' => 'email_greeting_custom',
     ]);
+    $rows = T\CiviCRM::lookup($rows, 'Contact', ['external_identifier' => 'external_identifier'], ['id']);
+    $rows = T\Columns::renameColumns($rows, ['id' => 'contact_id']);
     // Remap Acknowledgment Preference.
     $rows = T\ValueTransforms::valueMapper($rows, 'preferred_communication_method:label', ['Prefers email' => 'Email']);
     // Create any missing preferred communication methods in the option values table.
     $commMethods = T\RowFilters::getUniqueValues($rows, 'preferred_communication_method:label');
     T\CiviCRM::createOptionValues('preferred_communication_method', $commMethods);
     // Split 'Communication Tags' into relevant Civi fields.
-    // $rows = T\Transform::splitFieldToFields($rows, 'Communication Tags', ';');
-
-    // Handle Contacts with Alt. Addressee
-    // Determine if the Alt. Addressee value is different than the Addressee value.
-    $rows = T\Cleanup::compareAndSet($rows, 'Addressee', 'addressee_custom', '');
-    // Create seperate $rows variables based on the above comparison
-    $rowsWithAltAddressee = T\RowFilters::filterBlanks($rows, 'addressee_custom');
-    $rowsWithoutAltAddressee = array_diff($rows, $rowsWithAltAddressee);
-    // Merge rows back together.
-    $rows = $rowsWithoutAltAddressee + $rowsWithAltAddressee;
-
-    // Handle Contacts with Alt. Salutation
-    // Determine if the Alt. Salutation value is different than the Salutation value.
-    $rows = T\Cleanup::compareAndSet($rows, 'Salutation', 'email_greeting_custom', '');
-    // Create seperate $rows variables based on the above comparison
-    $rowsWithoutAltSalutation = T\RowFilters::filterBlanks($rows, 'email_greeting_custom');
-    $rowsWithAltSalutation = array_diff($rows, $rowsWithoutAltSalutation);
-    // Match the email greeting to the postal greeting.
-    $rowsWithAltSalutation = T\Columns::copyColumn($rowsWithAltSalutation, 'email_greeting_custom', 'postal_greeting_custom');
-    // Format the customized greetings with 'Dear '.
-    $rowsWithAltSalutation = T\Text::prependText($rows, 'email_greeting_custom', 'Dear ');
-    $rowsWithAltSalutation = T\Text::prependText($rows, 'addressee_custom', 'Dear ');
-    // Merge rows back together.
-    $rows = $rowsWithoutAltSalutation + $rowsWithAltSalutation;
-
-    // Clean up columns for rows without Alt. Salutation or Alt. Addressee.
-    $rowsWithoutAltSalutation = T\Columns::deleteColumns($rowsWithoutAltSalutation, ['email_greeting_custom', 'addressee_custom']);
-
+    $rows = T\Transform::splitFieldToRows($rows, 'Communication Tags', 'Communication Tag', ';');
+    $rows = T\RowFilters::filterBlanks($rows, 'Communication Tag');
+    T\Transform::denormalizeColumn($rows, 'contact_id', 'Communication Tag', ['Do not mail' => 'do_not_mail', 'Do not call' => 'do_not_phone', 'Do not email' => 'do_not_email']);
     return $rows;
   }
 
