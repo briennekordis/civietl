d53a66ea2669ed319375a6446f178c46ebd2df31
Jon Goldberg <jon@megaphonetech.com>
2023-05-09 17:57:40 -0400

    rewrite stepone to use API3 and skip greeting processing

--- src/Projects/Uaf/StepOne.php
+++ src/Projects/Uaf/StepOne.php
@@ -44,6 +44,27 @@ class StepOne {
     $rowsOrganization = T\Columns::newColumnWithConstant($rowsOrganization, 'last_name', '');
 
     $rows = $rowsIndividual + $rowsOrganization;
+    // Reduce to only the columns we care about.
+    $rows = T\Columns::deleteAllColumnsExcept($rows, [
+      'external_identifier',
+      'first_name',
+      'middle_name',
+      'last_name',
+      'nick_name',
+      'organization_name',
+      'contact_type',
+      'prefix_id:label',
+      'suffix_id:label',
+      'job_title',
+      'is_deceased',
+    ]);
+    // Get the prefix and suffix IDs so we can import via API3.
+    $rows = T\CiviCRM::OptionValuelookup($rows, 'individual_prefix', ['prefix_id:label' => 'label'], ['value']);
+    $rows = T\Columns::renameColumns($rows, ['value' => 'prefix_id']);
+    $rows = T\CiviCRM::OptionValuelookup($rows, 'individual_suffix', ['suffix_id:label' => 'label'], ['value']);
+    $rows = T\Columns::renameColumns($rows, ['value' => 'suffix_id']);
+    $rows = T\Columns::newColumnWithConstant($rows, 'skip_greeting_processing', 1);
+    $rows = T\Columns::deleteColumns($rows, ['prefix_id:label', 'suffix_id:label']);
     return $rows;
   }
 
