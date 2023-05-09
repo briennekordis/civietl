e2fa042d5dd3a631a515d4f964a9ce3153a66cea
Jon Goldberg <jon@megaphonetech.com>
2023-05-09 18:21:33 -0400

    add human readable timings

--- civietl.php
+++ civietl.php
@@ -8,6 +8,8 @@ require __DIR__ . '/vendor/autoload.php';
 use Civietl\Reader\ReaderService;
 use Civietl\Writer\WriterService;
 use Civietl\Utils as U;
+use DateTime;
+use DateTimeZone;
 
 $GLOBALS['newEtlStarted'] = TRUE;
 $cliArguments = U::parseCli();
@@ -19,6 +21,8 @@ eval(`cv --cwd=$webroot php:boot`);
 $importSettings = U::filterSteps($importSettings, $cliArguments);
 foreach ($importSettings as $stepName => $importSetting) {
   $time_start = microtime(TRUE);
+  $timeZone = new DateTimeZone('America/New_York');
+  $humanTimeStart = (new DateTime('now', $timeZone))->format('H:i:s');
   $importSetting += $importDefaults;
   $readerClassName = '\Civietl\Reader\\' . $importSetting['reader_type'];
   // $cacheClassName = '\Civietl\Cache\\' . $importSetting['cache_type'];
@@ -36,7 +40,8 @@ foreach ($importSettings as $stepName => $importSetting) {
   $writer = new WriterService(new $writerClassName($importSetting['writerOptions']));
   $writer->writeAll($rows);
   $time_end = microtime(TRUE);
+  $humanTimeEnd = (new DateTime('now', $timeZone))->format('H:i:s');
   $executionTime = round($time_end - $time_start, 2);
-  $timings = "$stepName: $executionTime seconds\n";
+  $timings = "$stepName: $executionTime seconds from $humanTimeStart - $humanTimeEnd\n";
   echo $timings;
 }
