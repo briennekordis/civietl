0dc21a8371a540167501ad25e66c648c5ef4749d
Brienne Kordis <brienne@megaphonetech.com>
2023-05-19 10:34:01 -0400

    change step to ContributionSoft

--- src/Projects/Uaf/ContributionsMatchingFlip.php
+++ src/Projects/Uaf/ContributionsMatchingFlip.php
@@ -11,6 +11,7 @@ class ContributionsMatchingFlip {
   public function transforms(array $rows) : array {
     // Remove columns that will not be imported.
     $rows = T\Columns::deleteAllColumnsExcept($rows, [
+      'LGL Constituent ID',
       'LGL Gift ID',
       'LGL Parent Gift ID',
       'Gift Amount',
@@ -27,12 +28,32 @@ class ContributionsMatchingFlip {
     $rowsWithVehicle = T\RowFilters::filterBlanks($rows, 'Vehicle Name');
     $rowsWithNoVehicle = array_diff_key($rows, $rowsWithVehicle);
     // Only import those without a Vehicle (Third Party Giving Contribution).
-    $rows = $rowsWithNoVehicle;
+    $rowsWithVehicle = T\CiviCRM::lookup($rowsWithVehicle, 'Contact', ['contact_id' => 'id'], ['contact_sub_type']);
+    // // Separate the rows in which the Contact is a Third Part Giving Vehicle. These Contributions will not be imported by the civietl.
+    $rowsWithThirdParty = array_filter($rowsWithVehicle, function($row) {
+      return isset($row['contact_sub_type'][0]) && $row['contact_sub_type'][0] === 'Third Party Giving Vehicle';
+    });
+    $rowsWithoutThirdParty = array_diff_key($rows, $rowsWithThirdParty);
+    $rows = $rowsWithNoVehicle + $rowsWithoutThirdParty;
+
+    // Look up and return the id of the Contact this Contribution is connected to.
+    $rows = T\CiviCRM::lookup($rows, 'Contact', ['LGL Constituent ID' => 'external_identifier'], ['id']);
+    $rows = T\Columns::renameColumns($rows, ['id' => 'contact_id']);
+    // Look up the Contact Type of the above Contact
+    $rows = T\CiviCRM::lookup($rows, 'Contact', ['contact_id' => 'id'], ['contact_type']);
+    // Separate the rows in which the Contact is an Individual. These Contributions will not be imported by the civietl.
+    $rowsWithIndividual = array_filter($rows, function($row) {
+      return isset($row['contact_type']) && $row['contact_type'] === 'Individual';
+    });
+    $rowsWithOrganization = array_diff_key($rows, $rowsWithIndividual);
+    $rows = $rowsWithOrganization;
+
+    // Assign the Contact of this Contribution as the Matching Gift Organization.
+    $rows = T\Columns::renameColumns($rows, ['contact_id' => 'gift_details.matching_gift']);
 
     // Connect the matching Contribution, to apply the Soft Credit.
     $rows = T\CiviCRM::lookup($rows, 'Contribution', ['LGL Parent Gift ID' => 'Legacy_Contribution_Data.LGL_Gift_ID'], ['id', 'contact_id']);
-    $rows = T\Columns::renameColumns($rows, ['id' => 'matching_gift.matching_gift_contribution']);
-    $rows = T\Columns::renameColumns($rows, ['contact_id' => 'matching_gift.matching_gift_contribution']);
+    $rows = T\Columns::renameColumns($rows, ['id' => 'contribution_id']);
 
     return $rows;
   }
